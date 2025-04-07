29d71cac343a9d233140245affef0662f64e1f27
fjramirez1 <fj.ramirezlopez@um.es>
2025-04-07 13:28:29 +0200

    Exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,7 +4,6 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-// Statistics component
 const Statistics = ({ good, neutral, bad }) => {
   if (good === 0 && neutral === 0 && bad === 0) {
     return <p>No feedback given</p>
