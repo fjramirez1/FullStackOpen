73b69ece1a95093dc69ea014feaaa8d66cd4f439
fjramirez1 <fj.ramirezlopez@um.es>
2025-04-21 12:35:47 +0200

    Exercise 2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,37 +1,39 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
+import Country from "./Country"
 
 const Countries = ({ countries }) => {
-    if (countries.length > 10) {
-        return <p>Too many matches, specify another filter</p>
-    }
-    if (countries.length > 1) {
-        return (
-          <ul>
-            {countries.map((country, index) => (
-              <li key={index}>{country.name.common}</li>
-            ))}
-          </ul>
-        )
-    }
-    if (countries.length === 1) {
-        const country = countries[0]
-        return (
-          <div>
-            <h2>{country.name.common}</h2>
-            <p>Capital: {country.capital}</p>
-            <p>Area: {country.area}</p>
-            <h3>Languages</h3>
-            <ul>
-              {Object.values(country.languages).map((language, index) => (
-                <li key={index}>{language}</li>
-              ))}
-            </ul>
-            <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
-          </div>
-        )
-    }
-    return null
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
+  useEffect(() => {
+    setSelectedCountry(null)
+  }, [countries])
+
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+
+  if (selectedCountry) {
+    return <Country country={selectedCountry} />
+  }
+
+  if (countries.length > 1) {
+    return (
+      <ul>
+        {countries.map((country, index) => (
+          <li key={index}>
+            {country.name.common}{" "}
+            <button onClick={() => setSelectedCountry(country)}>show</button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+  if (countries.length === 1) {
+    return <Country country={countries[0]} />
+  }
+
+  return null
 }
 
 export default Countries
---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Country = ({ country }) => {
+    return (
+        <div>
+          <h2>{country.name.common}</h2>
+          <p>Capital: {country.capital}</p>
+          <p>Area: {country.area}</p>
+          <h3>Languages</h3>
+          <ul>
+            {Object.values(country.languages).map((language, index) => (
+              <li key={index}>{language}</li>
+            ))}
+          </ul>
+          <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
+        </div>
+      )
+}
+
+export default Country
