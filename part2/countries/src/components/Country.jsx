517c1751025722773a6afd526e56379948e99aa2
fjramirez1 <fj.ramirezlopez@um.es>
2025-04-21 12:58:29 +0200

    Exercise 2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,20 +1,45 @@
 import React from "react"
+import { useEffect, useState } from "react"
 
 const Country = ({ country }) => {
-    return (
-        <div>
-          <h2>{country.name.common}</h2>
-          <p>Capital: {country.capital}</p>
-          <p>Area: {country.area}</p>
-          <h3>Languages</h3>
-          <ul>
-            {Object.values(country.languages).map((language, index) => (
-              <li key={index}>{language}</li>
-            ))}
-          </ul>
-          <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
-        </div>
-      )
+  const [weather, setWeather] = useState(null)
+  const api_key = import.meta.env.VITE_WEATHER_KEY
+  const capital = country.capital[0]
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`)
+      .then(response => response.json())
+      .then(data => setWeather(data))
+      console.log(weather);
+      
+  }, [capital, api_key])
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.values(country.languages).map((language, index) => (
+          <li key={index}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
+
+      {weather && (
+        <>
+          <h3>Weather in {capital}</h3>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>Wind: {weather.wind.speed} m/s</p>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Country
